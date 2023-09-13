f24e5272b72ca9dbe99064ed8191972edbf90174
María Victoria Bullrich <mvbullrich@gmail.com>
2023-09-13 10:57:39 -0300

    Update DAO.java

--- src/Model/DAO.java
+++ src/Model/DAO.java
@@ -11,5 +11,5 @@ public interface DAO<T>{
     public void eliminar(int id) throws DAOException;
     public void modificar(T elemento) throws DAOException;
     public T buscar(int id) throws DAOException;
-    //public ArrayList buscarTodos() throws DAOException;
+    public ArrayList buscarTodos() throws DAOException;
 }
